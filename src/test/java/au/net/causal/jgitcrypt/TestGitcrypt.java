19dccce50e1f2b88d541e2d1310a0f9a55f8fd27
Peter Runge <prunge@causal.net.au>
2024-07-22 22:20:32 +1000

    Generate test data as part of build.

--- src/test/java/au/net/causal/jgitcrypt/TestGitcrypt.java
+++ src/test/java/au/net/causal/jgitcrypt/TestGitcrypt.java
@@ -1,20 +1,16 @@
 package au.net.causal.jgitcrypt;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-@Disabled("manual run only")
 class TestGitcrypt
 {
     @Test
     void testKey() throws IOException
     {
-        try (InputStream is = Files.newInputStream(Path.of("testdocker/export/thekey")))
+        try (InputStream is = TestGitcrypt.class.getResourceAsStream("/testkey/thekey"))
         {
             GitcryptKey key = GitcryptKey.read(is);
             System.out.println(key);
@@ -25,17 +21,16 @@ class TestGitcrypt
     void testEverything() throws Exception
     {
         GitcryptKey key;
-        try (InputStream is = Files.newInputStream(Path.of("testdocker/export/thekey")))
+        try (InputStream is = TestGitcrypt.class.getResourceAsStream("/testkey/thekey"))
         {
             key = GitcryptKey.read(is);
         }
 
         GitcryptDecoder decoder = new GitcryptDecoder(key);
 
-        try (InputStream is = decoder.decode(Files.newInputStream(Path.of("testdocker/export/repo/secrets.txt"))))
+        try (InputStream is = decoder.decode(TestGitcrypt.class.getResourceAsStream("/testrepo/secrets.txt")))
         {
             is.transferTo(System.out);
         }
     }
-
 }
