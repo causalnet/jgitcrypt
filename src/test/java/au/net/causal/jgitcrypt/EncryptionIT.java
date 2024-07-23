ebcaa74c5a50486c34f85e13602c75480ca86694
Peter Runge <prunge@causal.net.au>
2024-07-23 23:19:21 +1000

    More work on encryption tests.

--- src/test/java/au/net/causal/jgitcrypt/EncryptionIT.java
+++ src/test/java/au/net/causal/jgitcrypt/EncryptionIT.java
@@ -2,20 +2,43 @@ package au.net.causal.jgitcrypt;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.util.FileUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 class EncryptionIT
 {
     @Test
     void doEncryption()
-    throws GitAPIException
+    throws GitAPIException, IOException
     {
-        Git git = Git.init()
-                .setDirectory(Path.of("mygitrepo").toFile())
+        Path repoDir = Path.of("target", "verifier-docker-data", "it-gitrepo");
+        FileUtils.delete(repoDir.toFile(), FileUtils.RECURSIVE | FileUtils.RETRY | FileUtils.SKIP_MISSING);
+
+        try (Git git = Git.init()
+                .setDirectory(repoDir.toFile())
                 .setInitialBranch("main")
-                .call();
+                .call())
+        {
+            //Encrypted file
+            Path encryptedFile = repoDir.resolve("newsecrets.txt");
+            //TODO actually encrypt it
+            Files.writeString(encryptedFile, "This file should be encrypted.");
+            git.add().addFilepattern(encryptedFile.getFileName().toString()).call();
+
+            //.gitattributes file
+            Path gitAttributesFile = repoDir.resolve(".gitattributes");
+            Files.writeString(gitAttributesFile, encryptedFile.getFileName().toString() + " filter=git-crypt diff=git-crypt");
+            git.add().addFilepattern(gitAttributesFile.getFileName().toString()).call();
+
+            git.commit()
+                    .setMessage("A commit.")
+                    .setCommitter("Someone", "someone@example.com")
+                    .call();
+        }
 
         System.out.println("Do encryption so that the result can get picked up by gitcrypt run inside docker");
     }
