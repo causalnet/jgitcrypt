ad8e705c6f78e66c85cd830d592c23dcf2cb6f8e
Peter Runge <prunge@causal.net.au>
2024-07-23 19:02:10 +1000

    Make the git repo for verification in Java.

--- src/test/java/au/net/causal/jgitcrypt/EncryptionIT.java
+++ src/test/java/au/net/causal/jgitcrypt/EncryptionIT.java
@@ -1,12 +1,22 @@
 package au.net.causal.jgitcrypt;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 class EncryptionIT
 {
     @Test
     void doEncryption()
+    throws GitAPIException
     {
+        Git git = Git.init()
+                .setDirectory(Path.of("mygitrepo").toFile())
+                .setInitialBranch("main")
+                .call();
+
         System.out.println("Do encryption so that the result can get picked up by gitcrypt run inside docker");
     }
 }
