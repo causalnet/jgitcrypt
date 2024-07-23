835d33dee221b4f1ba3a82f80e8bb5983f7bc56f
Peter Runge <prunge@causal.net.au>
2024-07-23 18:23:34 +1000

    Set up testing infrastructure for verifying encryption from docker.

---
+++ src/test/java/au/net/causal/jgitcrypt/EncryptionIT.java
@@ -0,0 +1,12 @@
+package au.net.causal.jgitcrypt;
+
+import org.junit.jupiter.api.Test;
+
+class EncryptionIT
+{
+    @Test
+    void verifySomething()
+    {
+        System.out.println("Verification goes here");
+    }
+}
---
+++ src/test/java/au/net/causal/jgitcrypt/EncryptionVerify.java
@@ -0,0 +1,12 @@
+package au.net.causal.jgitcrypt;
+
+import org.junit.jupiter.api.Test;
+
+class EncryptionVerify
+{
+    @Test
+    void verifyAfterDockerRun()
+    {
+        System.out.println("This should run after docker");
+    }
+}
