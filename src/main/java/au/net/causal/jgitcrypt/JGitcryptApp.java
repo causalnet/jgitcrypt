570c2767f5fe3388f00ffabbec25e77e363a6483
Peter Runge <prunge@causal.net.au>
2024-07-27 17:00:00 +1000

    Start work on very simple command line app.

---
+++ src/main/java/au/net/causal/jgitcrypt/JGitcryptApp.java
@@ -0,0 +1,15 @@
+package au.net.causal.jgitcrypt;
+
+public class JGitcryptApp
+{
+    public static void main(String... args)
+    {
+        //Very simple syntax
+        //First item is command (decrypt/encrypt)
+        //Second item is key file
+        //Third item (optional) is input file to encrypt or decrypt (defaults to stdin)
+        //Fourth item (optional) is output file to encrypt/decrypt to (defaults to stdout)
+
+        System.out.println("TODO command line app");
+    }
+}
