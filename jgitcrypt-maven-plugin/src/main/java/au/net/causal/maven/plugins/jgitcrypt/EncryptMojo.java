3877f9a31656db20c3d69273f1b0b1284c93410d
Peter Runge <prunge@causal.net.au>
2024-07-30 19:05:19 +1000

    Add a goal to encrypt a file.

---
+++ jgitcrypt-maven-plugin/src/main/java/au/net/causal/maven/plugins/jgitcrypt/EncryptMojo.java
@@ -0,0 +1,65 @@
+package au.net.causal.maven.plugins.jgitcrypt;
+
+import au.net.causal.jgitcrypt.GitcryptDecoder;
+import au.net.causal.jgitcrypt.GitcryptKey;
+import au.net.causal.jgitcrypt.GitcryptSecurityException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+@Mojo(name="encrypt")
+public class EncryptMojo extends AbstractMojo
+{
+    @Parameter(property = "jgitcrypt.key.file", defaultValue = "${project.build.directory}/gitcrypt.key", required = true)
+    protected File keyFile;
+
+    @Parameter(property = "jgitcrypt.source.file", required = true)
+    protected File sourceFile;
+
+    @Parameter(property = "jgitcrypt.target.file", required = true)
+    protected File targetFile;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        if (!keyFile.exists())
+            throw new MojoExecutionException("Key file " + keyFile.getAbsolutePath() + " not found.");
+        if (!sourceFile.exists())
+            throw new MojoExecutionException("File to encrypt " + sourceFile + " not found.");
+
+        getLog().info("Encrypting " + sourceFile.getAbsolutePath() +
+                " to " + targetFile.getAbsolutePath() +
+                " using key " + keyFile.getAbsolutePath());
+
+        //Load the key
+        GitcryptKey key;
+        try (InputStream is = Files.newInputStream(keyFile.toPath()))
+        {
+            key = GitcryptKey.read(is);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+        GitcryptDecoder decoder = new GitcryptDecoder(key);
+
+        try (InputStream is = Files.newInputStream(sourceFile.toPath());
+             OutputStream os = Files.newOutputStream(targetFile.toPath()))
+        {
+            decoder.encode(is, os);
+        }
+        catch (IOException | GitcryptSecurityException e)
+        {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+}
